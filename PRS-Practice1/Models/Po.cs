4ae1514717a9592a73e39ef3e8c990a8805aba84
Jacob Witte <wittejg@gmail.com>
2023-03-21 15:04:02 -0400

    *update Po and Poline classes

--- PRS-Practice1/Models/Po.cs
+++ PRS-Practice1/Models/Po.cs
@@ -1,7 +1,7 @@
 ﻿namespace PRS_Practice1.Models {
     public class Po {
-        public Vendor Vendor { get; set; }
-        public IEnumerable<Poline> Polines { get; set; }
+        public Vendor Vendor { get; set; } = null!;
+        public IEnumerable<Poline> Polines { get; set; } = null!;
         public decimal PoTotal { get; set; }
     }
 }
--- PRS-Practice1/Models/Poline.cs
+++ PRS-Practice1/Models/Poline.cs
@@ -1,7 +1,7 @@
 ﻿namespace PRS_Practice1.Models {
     public class Poline {
-        public string Product { get; set; }
-        public int Quantity { get; set; }
+        public string Product { get; set; } = null!;
+        public int Quantity { get; set; } 
         public decimal Price { get; set; }
         public decimal LineTotal { get; set; }
 
